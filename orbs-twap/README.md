0859bd8b77535cf2e4bc90851e1a2896c0b35247
idanatar <idana@orbs.com>
2024-04-24 16:00:34 +0300

    handleOrderCreated for TWAP

--- orbs-twap/README.md
+++ orbs-twap/README.md
@@ -41,6 +41,45 @@ Accumulated trading volume
 - cumulativeTotalCalculatedValue: BigDecimal!
 - totalCount: Int
 
+### OrderCreated
+- id: Bytes!
+- Contract_id: BigInt! # uint64
+- maker: Bytes! # address
+- exchange: Bytes! # address
+- ask_exchange: Bytes! # address
+- ask_srcToken: Bytes! # address
+- ask_dstToken: Bytes! # address
+- ask_srcAmount: BigInt! # uint256
+- ask_srcBidAmount: BigInt! # uint256
+- ask_dstMinAmount: BigInt! # uint256
+- ask_deadline: BigInt! # uint32
+- ask_bidDelay: BigInt! # uint32
+- ask_fillDelay: BigInt! # uint32
+- ask_data: Bytes! # bytes 
+- blockNumber: BigInt!
+- blockTimestamp: BigInt!
+- transactionHash: Bytes!
+- srcTokenSymbol: String 
+- dstTokenSymbol: String 
+- dollarValueIn: BigDecimal!
+- timestamp: String!
+- dex: String!
+
+### CreatedDaily
+- id: String!
+- date: String!
+- dailyCount: Int 
+- dex: String!
+
+### CreatedTotal
+- id: String!
+- totalCount: Int
+
+### DailyActiveUsers 
+- id: String!
+- userAddresses: [String!]!
+- count: Int!
+
 ## Building and deploying
 1. Create a new subgraph via the studio web UI.
 2. Authenticate in CLI: `graph auth --studio <key>`
@@ -57,7 +96,63 @@ Deploys the subgraph for particular network to the official Graph Node. Runs the
 To utilize this subgraph, deploy it to a suitable indexing service such as The Graph, and query the generated GraphQL API to retrieve analytics data related to dTWAP protocol swaps.
 
 ### Examples
-#### Select by order ID
+#### Orders created on 2023-06-21
+```graphql
+{
+    orderCreateds(where:{timestamp_contains:"2023-06-21"}) {
+        id
+        Contract_id
+        ask_bidDelay
+        ask_data
+        ask_deadline
+        ask_dstMinAmount
+        ask_dstToken
+        ask_fillDelay
+        ask_exchange
+        ask_srcToken
+        ask_srcBidAmount
+        ask_srcAmount
+        blockNumber
+        blockTimestamp
+        dex
+        dollarValueIn
+        dstTokenSymbol
+        exchange
+        maker
+        srcTokenSymbol
+        timestamp
+        transactionHash
+    }
+}
+```
+#### Get orders creation volume by day for Thena
+```graphql
+{
+  createdDailies(where:{dex: "Thena"}) {
+    id
+    dailyCount
+  }
+}
+```
+#### Get accumulated order creations
+```graphql
+{
+    createdTotals {
+        id
+        totalCount
+    }
+}
+```
+#### Get DAUs for PancakeSwap
+```graphql
+{
+    dailyActiveUsers_collection(where:{id_starts_with:"PancakeSwap"}) {
+        id
+        count
+    }
+}
+```
+#### Select Filled order by order ID
 ```graphql
 {
   orderFilleds(where:{TWAP_id:445}) {
--- orbs-twap/src/twap.ts
+++ orbs-twap/src/twap.ts
@@ -1,8 +1,8 @@
-import {Address, BigDecimal} from '@graphprotocol/graph-ts'
+import {Address, BigDecimal, log} from '@graphprotocol/graph-ts'
 import {fetchTokenSymbol, fetchUSDValue, formatTimestamp,} from "./utils/utils"
-import {TWAP_ADDRESS, getDexByRouter, UNIQUE_USERS_TODAY_ID} from "./utils/constants";
-import {OrderFilled as OrderFilledEvent, TWAP} from "../generated/TWAP/TWAP"
-import {OrderFilled, FilledDaily, FilledTotal, DailyActiveUsers, UniqueUsersToday} from "../generated/schema"
+import {TWAP_ADDRESS, getDexByRouter} from "./utils/constants";
+import {OrderFilled as OrderFilledEvent, OrderCreated as OrderCreatedEvent, TWAP} from "../generated/TWAP/TWAP"
+import {OrderFilled, FilledDaily, FilledTotal, DailyActiveUsers, OrderCreated, CreatedDaily, CreatedTotal} from "../generated/schema"
 
 export function handleOrderFilled(event: OrderFilledEvent): void {
   let entity = new OrderFilled(
@@ -72,26 +72,86 @@ export function handleOrderFilled(event: OrderFilledEvent): void {
     total.totalCount += 1
   }
   total.save()
+}
 
-  // handle DAU - each day we build a new list of unique users and count them
-  const userAddress = entity.userAddress.toHexString()
-  let usersToday = UniqueUsersToday.load(UNIQUE_USERS_TODAY_ID)
-  if (usersToday == null) {
-    usersToday = new UniqueUsersToday(UNIQUE_USERS_TODAY_ID)
-  }
+export function handleOrderCreated(event: OrderCreatedEvent): void {
+  let entity = new OrderCreated(
+      event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.Contract_id = event.params.id
+  entity.maker = event.params.maker
+  entity.exchange = event.params.exchange
+  entity.ask_exchange = event.params.ask.exchange
+  entity.ask_srcToken = event.params.ask.srcToken
+  entity.ask_dstToken = event.params.ask.dstToken
+  entity.ask_srcAmount = event.params.ask.srcAmount
+  entity.ask_srcBidAmount = event.params.ask.srcBidAmount
+  entity.ask_dstMinAmount = event.params.ask.dstMinAmount
+  entity.ask_deadline = event.params.ask.deadline
+  entity.ask_bidDelay = event.params.ask.bidDelay
+  entity.ask_fillDelay = event.params.ask.fillDelay
+  entity.ask_data = event.params.ask.data
 
-  let dau = DailyActiveUsers.load(day)
-  if (dau == null) { // a new day - override usersToday
-    dau = new DailyActiveUsers(day)
-    dau.count = 1
-    usersToday.userAddresses = [userAddress]
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.timestamp = formatTimestamp(event.block.timestamp)
+
+  entity.srcTokenSymbol = fetchTokenSymbol(Address.fromBytes(entity.ask_srcToken))
+  entity.dstTokenSymbol = fetchTokenSymbol(Address.fromBytes(entity.ask_dstToken))
+  entity.dex = getDexByRouter(entity.exchange.toHexString())
+
+  entity.dollarValueIn = fetchUSDValue(entity.srcTokenSymbol!, entity.ask_srcToken.toHexString()) * BigDecimal.fromString(entity.ask_srcAmount.toString())
+
+  entity.save()
+
+  const day = entity.timestamp.slice(0, 10)
+  const key = `${entity.dex}_${day}`
+
+  // store daily volume
+  let daily = CreatedDaily.load(key)
+  if (daily == null) {
+    daily = new CreatedDaily(key)
+    daily.date = day
+    // daily.dailyTotalCalculatedValue = entity.dollarValueIn
+    daily.dailyCount = 1
+    daily.dex = entity.dex
   }
   else {
-    if (!usersToday.userAddresses.includes(userAddress)) {
-      usersToday.userAddresses.push(userAddress)
+    // daily.dailyTotalCalculatedValue = daily.dailyTotalCalculatedValue + entity.dollarValueIn
+    daily.dailyCount += 1
+  }
+  daily.save()
+
+  // store cumulative volume
+  let total = CreatedTotal.load(entity.dex)
+  if (total == null) {
+    total = new CreatedTotal(entity.dex)
+    // total.cumulativeTotalCalculatedValue = entity.dollarValueIn
+    total.totalCount = 1
+  }
+  else {
+    // total.cumulativeTotalCalculatedValue = total.cumulativeTotalCalculatedValue + entity.dollarValueIn
+    total.totalCount += 1
+  }
+  total.save()
+
+  // DAUs
+  const userAddress = entity.maker.toHexString()
+  let dau = DailyActiveUsers.load(key)
+  if (dau == null) {
+    dau = new DailyActiveUsers(key)
+    dau.count = 1
+    dau.userAddresses = [userAddress]
+  }
+  else {
+    if (!dau.userAddresses.includes(userAddress)) {
       dau.count += 1
+      let userAddresses = dau.userAddresses
+      userAddresses.push(userAddress)
+      dau.userAddresses = userAddresses
     }
   }
-  usersToday.save()
   dau.save()
 }
