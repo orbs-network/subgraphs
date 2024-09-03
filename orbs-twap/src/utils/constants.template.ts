99e422012f8ddafe1c37063168e4eb771ebdd2cd
idanatar <idana@orbs.com>
2024-09-03 12:10:16 +0300

    Added mainnet twap

--- orbs-twap/src/utils/constants.template.ts
+++ orbs-twap/src/utils/constants.template.ts
@@ -41,6 +41,7 @@ export function getDexByRouter(routerAddress: string): string { // use only lowe
     if (routerAddress == "0x26d0ec4be402bce03aaa8aaf0cf67e9428ba54ef") return "QuickSwap"
     if (routerAddress == "0xefe1b6096838949156e5130604434a2a13c68c68") return "BaseSwap"
     if (routerAddress == "0x10695e3c265639e46d45c7bb427a4a4fd449af1e") return "SushiSwap"
+    if (routerAddress == "0xc55943fa6509004b2903ed8f8ab7347bfc47d0ba") return "SushiSwap"
     if (routerAddress == "0x8ffde23fba2d7aea9c3cbf2d5b7b533bb46754a8") return "Arbidex"
     if (routerAddress == "0xceff098c9199c5d9cf24078dc14eb8f787631cc0") return "Chronos"
     return "Unknown"
@@ -99,5 +100,8 @@ export function getOracleAddress(asset: string): string | null {
     if (asset == "TRX") return '{{trx}}'
     if (asset == "UNI") return '{{uni}}'
     if (asset == "XRP") return '{{xrp}}'
+    if (asset == "TAO") return '{{tao}}'
+    if (asset == "ENS") return '{{ens}}'
+    if (asset == "STETH") return '{{steth}}'
     return null;
 }
